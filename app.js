6a95a5906431ea5babd674c8875eba345804224b
Marat Zabirov <marat.zabirov@orioninc.com>
2020-08-17 16:18:31 +0300

    add express

--- app.js
+++ app.js
@@ -1,39 +1,95 @@
-var port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs'),
-    html = fs.readFileSync('index.html');
+/**
+ * Module dependencies.
+ */
 
-var log = function(entry) {
-    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
-};
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , hike = require('./routes/hike')
+  , http = require('http')
+  , path = require('path')
+  , mysql = require('mysql')
+  , async = require('async');
 
-var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        var body = '';
+var app = express();
 
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
-
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
-
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
-        });
-    } else {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
-    }
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
-// Listen on port 3000, IP defaults to 127.0.0.1
-server.listen(port);
+app.configure('development', function() {
+  console.log('Using development settings.');
+  app.set('connection', mysql.createConnection({
+    host: '',
+    user: '',
+    port: '',
+    password: ''}));
+  app.use(express.errorHandler());
+});
 
-// Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port + '/');
+app.configure('production', function() {
+  console.log('Using production settings.');
+  app.set('connection', mysql.createConnection({
+    host: process.env.RDS_HOSTNAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    port: process.env.RDS_PORT}));
+});
+
+function init() {
+  app.get('/', routes.index);
+  app.get('/users', user.list);
+  app.get('/hikes', hike.index);
+  app.post('/add_hike', hike.add_hike);
+
+  http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+  });
+}
+
+var client = app.get('connection');
+async.series([
+  function connect(callback) {
+    client.connect(callback);
+  },
+  function clear(callback) {
+    client.query('DROP DATABASE IF EXISTS mynode_db', callback);
+  },
+  function create_db(callback) {
+    client.query('CREATE DATABASE mynode_db', callback);
+  },
+  function use_db(callback) {
+    client.query('USE mynode_db', callback);
+  },
+  function create_table(callback) {
+     client.query('CREATE TABLE HIKES (' +
+                         'ID VARCHAR(40), ' +
+                         'HIKE_DATE DATE, ' +
+                         'NAME VARCHAR(40), ' +
+                         'DISTANCE VARCHAR(40), ' +
+                         'LOCATION VARCHAR(40), ' +
+                         'WEATHER VARCHAR(40), ' +
+                         'PRIMARY KEY(ID))', callback);
+  },
+  function insert_default(callback) {
+    var hike = {HIKE_DATE: new Date(), NAME: 'Hazard Stevens',
+          LOCATION: 'Mt Rainier', DISTANCE: '4,027m vertical', WEATHER:'Bad'};
+    client.query('INSERT INTO HIKES set ?', hike, callback);
+  }
+], function (err, results) {
+  if (err) {
+    console.log('Exception initializing database.');
+    throw err;
+  } else {
+    console.log('Database initialization complete.');
+    init();
+  }
+});
